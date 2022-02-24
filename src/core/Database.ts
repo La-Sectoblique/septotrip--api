2ce359c0044c6ed772c932005e30ee2c920f75fe
Tanguy Gimenez <gimeneztanguy@gmail.com>
2022-02-24 09:37:04 +0100

    fix(database): changes on .env variables

--- src/core/Database.ts
+++ src/core/Database.ts
@@ -7,22 +7,22 @@ const MODEL_FOLDER = path.join(__dirname, "..", "models");
 
 const isDev = process.env.NODE_ENV !== "production";
 
-if(!process.env.POSTGRES_USERNAME || !process.env.POSTGRES_PASSWORD)
+if(!process.env.POSTGRES_USER || !process.env.POSTGRES_PASSWORD)
 	throw new Error("No credentials provided");
 
 if(!process.env.POSTGRES_HOST || !process.env.POSTGRES_PORT)
 	throw new Error("No address or port provided");
 
-if(!process.env.POSTGRES_DATABASE) 
+if(!process.env.POSTGRES_DB) 
 	throw new Error("No database provided");
 
 const sequelize = new Sequelize({
 	dialect: "postgres",
-	username: process.env.POSTGRES_USERNAME,
+	username: process.env.POSTGRES_USER,
 	password: process.env.POSTGRES_PASSWORD,
 	host: process.env.POSTGRES_HOST,
 	port: Number.parseInt(process.env.POSTGRES_PORT),
-	database: process.env.POSTGRES_DATABASE,
+	database: process.env.POSTGRES_DB,
 	logging: (msg) => Loggers.getLogger("database").debug(msg)
 });
 
