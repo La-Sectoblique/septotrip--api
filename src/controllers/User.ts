ba20de4dc3dda743bf9f9cdbb6c31e36ef1de264
Tanguy Gimenez <gimeneztanguy@gmail.com>
2022-02-25 14:46:14 +0100

    feat(login): return created user during register

--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { User } from "../models/User";
 import InvalidBodyError from "../types/errors/InvalidBodyError";
 import { isUserInput } from "../types/models/User";
 
@@ -9,6 +10,8 @@ export async function register(request: Request, response: Response, next: NextF
 		next({ message: "Invalid request body", code: 400, name: "InvalidBodyError" } as InvalidBodyError);
 	}
 	else {
-		console.log(request.body);
+		const user = await User.create(request.body);
+		response.json(user);
+		return;
 	}
 }
