c3639a81c11f20ebeb79fb5327c45c4d61a8e2eb
Tanguy Gimenez <gimeneztanguy@gmail.com>
2022-03-10 11:36:19 +0100

    feat(point): points modifications

--- src/routes/protected/points.ts
+++ src/routes/protected/points.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
-import { addPoint, getPointsByUser } from "../../controllers/Point";
+import { addPoint, getPointsByUser, removePoint, updatePoint } from "../../controllers/Point";
 import MethodNotAllowed from "../../middlewares/MethodNotAllowed";
+import { LoadPoint } from "../../middlewares/PointLoader";
 
 const router = Router();
 
 router.route("/points")
 	.get(getPointsByUser)
 	.post(addPoint)
+	.all(MethodNotAllowed);
+
+router.route("/points/:id")
+	.all(LoadPoint)
+	.delete(removePoint)
+	.put(updatePoint)
 	.all(MethodNotAllowed);
 
 export default router;
