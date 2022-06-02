5dc89da41cebefe4a6ee894a95ae84be8576828b
Tanguy Gimenez <gimeneztanguy@gmail.com>
2022-06-02 11:50:21 +0200

    feat(Path): making description optionnal

--- src/models/Path.ts
+++ src/models/Path.ts
@@ -5,7 +5,7 @@ import { Step } from "./Step";
 export class Path extends Model<PathAttributes, PathInput> implements PathAttributes {
 	declare id: number;
 	
-	declare description: string;
+	declare description?: string;
 
 	declare destinationId: number;
 	declare destination: NonAttribute<Step>;
@@ -15,7 +15,7 @@ export function init(sequelize: Sequelize): void {
 	Path.init({
 		description: {
 			type: DataTypes.STRING,
-			allowNull: false
+			allowNull: true
 		},
 		destinationId: {
 			type: DataTypes.INTEGER,
